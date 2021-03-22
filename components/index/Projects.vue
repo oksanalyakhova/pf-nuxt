e89af23d45a2df2222d09ef13f43a50768f7277b
Oksana Lyakhova <oksana.lyahova@coderiver.com.ua>
2021-03-22 16:47:37 +0200

    cleaned projects scroll anim

--- components/index/Projects.vue
+++ components/index/Projects.vue
@@ -1,13 +1,7 @@
 <template lang="pug">
   .projects
-    .projects-trigger(
-      ref="projectsTrigger"
-    )
-    ul.projects-list(
-      ref="projects"
-    )
+    ul.projects-list
       li.project(
-        ref="project"
         v-for="project in projects"
         :key="project.label"
       )
@@ -20,10 +14,6 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import ProjectItem from './partials/ProjectItem';
-import {gsap} from 'gsap/dist/gsap';
-import ScrollTrigger from 'gsap/dist/ScrollTrigger.js';
-
-gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   components: {
@@ -34,51 +24,9 @@ gsap.registerPlugin(ScrollTrigger);
       type: Array,
       default: () => []
     },
-  },
-  data() {
-    return {
-      vh: window.innerHeight
-    }
   }
 })
-export default class Projects extends Vue {
-  created() {
-    this.setWindowSizes();
-  }
-  mounted() {
-    this.$nextTick(() => {
-      this.scrollAnim();
-      window.addEventListener('resize', this.setWindowSizes);
-    })
-  }
-  destroyed() {
-    window.removeEventListener('resize', this.setWindowSizes);
-  }
-  setWindowSizes() {
-    this.vh = window.innerHeight;
-  }
-  scrollAnim() {
-    // const actionStart = gsap.timeline({
-    //   scrollTrigger: {
-    //     trigger: this.$refs.projectsTrigger,
-    //     pin: true,
-    //     scrub: 0.3,
-    //     start: "top 25%",
-    //     end: '+=' + this.vh,
-    //     invalidateOnRefresh: true
-    //   }
-    // })
-    //   .to(this.$refs.projects, {
-    //     y: -100,
-    //     duration: 1,
-    //     ease: 'none'
-    //   }, 'spin')
-    //
-    // window.addEventListener('resize', () => {
-    //   ScrollTrigger.refresh()
-    // });
-  }
-}
+export default class Projects extends Vue {}
 </script>
 
 <style lang="sass">
@@ -88,14 +36,6 @@ export default class Projects extends Vue {
   position: relative
   padding: 30vh 6.25vw
   background-color: $c-grey
-
-  .projects-trigger
-    position: absolute
-    left: 0
-    top: -200vh
-    width: 100%
-    height: 2px
-    background-color: red
 
   .project
     margin: 0 0 10vw
