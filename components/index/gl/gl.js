beee0abd78b4d68205b37fb6a9c897228daebbc8
Oksana Lyakhova <oksana.lyahova@coderiver.com.ua>
2021-03-25 10:27:52 +0200

    fixed shaders

--- components/index/gl/gl.js
+++ components/index/gl/gl.js
@@ -11,141 +11,59 @@ const vertexShader = `
     varying vec2 vUv;
     varying float vWave;
     uniform float uTime;
+    uniform float uProg;
 
-    //
-    // Description : Array and textureless GLSL 2D/3D/4D simplex
-    //               noise functions.
-    //      Author : Ian McEwan, Ashima Arts.
-    //  Maintainer : ijm
-    //     Lastmod : 20110822 (ijm)
-    //     License : Copyright (C) 2011 Ashima Arts. All rights reserved.
-    //               Distributed under the MIT License. See LICENSE file.
-    //               https://github.com/ashima/webgl-noise
-    //
-
-    vec3 mod289(vec3 x) {
-      return x - floor(x * (1.0 / 289.0)) * 289.0;
-    }
-
-    vec4 mod289(vec4 x) {
-      return x - floor(x * (1.0 / 289.0)) * 289.0;
-    }
-
-    vec4 permute(vec4 x) {
-         return mod289(((x*34.0)+1.0)*x);
-    }
-
-    vec4 taylorInvSqrt(vec4 r)
-    {
-      return 1.79284291400159 - 0.85373472095314 * r;
-    }
-
-    float snoise(vec3 v) {
-      const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
-      const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
-
-      // First corner
-      vec3 i  = floor(v + dot(v, C.yyy) );
-      vec3 x0 =   v - i + dot(i, C.xxx) ;
-
-      // Other corners
-      vec3 g = step(x0.yzx, x0.xyz);
-      vec3 l = 1.0 - g;
-      vec3 i1 = min( g.xyz, l.zxy );
-      vec3 i2 = max( g.xyz, l.zxy );
-
-      vec3 x1 = x0 - i1 + C.xxx;
-      vec3 x2 = x0 - i2 + C.yyy; // 2.0*C.x = 1/3 = C.y
-      vec3 x3 = x0 - D.yyy;      // -1.0+3.0*C.x = -0.5 = -D.y
-
-      // Permutations
-      i = mod289(i);
-      vec4 p = permute( permute( permute(
-                 i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
-               + i.y + vec4(0.0, i1.y, i2.y, 1.0 ))
-               + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
-
-      // Gradients: 7x7 points over a square, mapped onto an octahedron.
-      // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
-      float n_ = 0.142857142857; // 1.0/7.0
-      vec3  ns = n_ * D.wyz - D.xzx;
-
-      vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,7*7)
-
-      vec4 x_ = floor(j * ns.z);
-      vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
-
-      vec4 x = x_ *ns.x + ns.yyyy;
-      vec4 y = y_ *ns.x + ns.yyyy;
-      vec4 h = 1.0 - abs(x) - abs(y);
-
-      vec4 b0 = vec4( x.xy, y.xy );
-      vec4 b1 = vec4( x.zw, y.zw );
-
-      vec4 s0 = floor(b0)*2.0 + 1.0;
-      vec4 s1 = floor(b1)*2.0 + 1.0;
-      vec4 sh = -step(h, vec4(0.0));
-
-      vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
-      vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
-
-      vec3 p0 = vec3(a0.xy,h.x);
-      vec3 p1 = vec3(a0.zw,h.y);
-      vec3 p2 = vec3(a1.xy,h.z);
-      vec3 p3 = vec3(a1.zw,h.w);
-
-      // Normalise gradients
-      vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
-      p0 *= norm.x;
-      p1 *= norm.y;
-      p2 *= norm.z;
-      p3 *= norm.w;
-
-      // Mix final noise value
-      vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
-      m = m * m;
-      return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1),
-                                    dot(p2,x2), dot(p3,x3) ) );
-    }
+    // #pragma glslify: noise = require(glsl-noise/simplex/3d)
 
     void main() {
-      vUv = uv;
-
       vec3 pos = position;
-      float noiseFreq = 3.5;
-      float noiseAmp = 0.15;
-      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
-      pos.z += snoise(noisePos) * noiseAmp;
+
+      // pos.z += noise(vec3(pos.x * 4. + uTime, pos.y, 0.)) * uProg;
       vWave = pos.z;
+      pos.z *= 3.;
+
+      vUv = uv;
 
       gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);
     }
   `
+
 const fragmentShader = `
     precision mediump float;
 
     varying vec2 vUv;
     varying float vWave;
     uniform sampler2D uTexture;
+    uniform float uTime;
+    uniform float uProg;
 
     void main() {
-      float wave = vWave * 0.25;
-      float r = texture2D(uTexture, vUv).r;
-      float g = texture2D(uTexture, vUv).g;
-      float b = texture2D(uTexture, vUv + wave).b;
+      vec2 uv = vUv;
+      vec2 dUv = vec2(uv.x, uv.y);
+      // vec3 texture;
+
+      float w = vWave;
+      float r = texture2D(uTexture, dUv + vec2(0., 0.) + uProg * w * 0.05).r;
+      float g = texture2D(uTexture, dUv + vec2(0., 0.) + uProg * w * 0.0).g;
+      float b = texture2D(uTexture, dUv + vec2(0., 0.) + uProg * w * -0.02).b;
       vec3 texture = vec3(r, g, b);
       gl_FragColor = vec4(texture, 1.);
     }
+
   `
 
 @Component
 export default class Gl extends Vue {
   img = 'img/o.png';
+
+  loader = {};
   scene = {};
   camera = {};
   renderer = {};
   clock = {};
   controls = {};
+  texture = {};
+  mesh = {};
 
   mounted() {
     this.scene = new THREE.Scene();
@@ -159,41 +77,74 @@ export default class Gl extends Vue {
       canvas: document.querySelector(".canvas"),
       antialias: true
     });
+
     this.clock = new THREE.Clock();
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
-    this.camera.position.z = 1;
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.setClearColor(0x000000, 1);
+    this.geometry = new THREE.PlaneBufferGeometry(1, 1, 32, 32);
+    this.material = new THREE.ShaderMaterial({
+      vertexShader: glsl(vertexShader),
+      fragmentShader: glsl(fragmentShader),
+    });
+    this.loader = new THREE.TextureLoader();
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    // this.camera = new THREE.PerspectiveCamera(
+    //   45,
+    //   window.innerWidth / window.innerHeight,
+    //   0.1,
+    //   100
+    // );
+    // this.renderer = new THREE.WebGLRenderer({
+    //   canvas: document.querySelector(".canvas"),
+    //   antialias: true
+    // });
+    // this.clock = new THREE.Clock();
+    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    //
+    // this.camera.position.z = 1;
+    // this.renderer.setSize(window.innerWidth, window.innerHeight);
+    // this.renderer.setClearColor(0x000000, 1);
 
     this.onResize();
     this.init();
   }
 
   init() {
-    this.createMesh();
+    this.camera.position.z = 1;
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setClearColor(0x000000, 1);
+
+    this.material.uniforms = {
+      uTexture: { value: 0 },
+      uTime: { value: 0 },
+      uProg: { value: 0 },
+    }
+
+    this.texture = this.loader.load(this.img, (texture) => {
+      texture.minFilter = THREE.LinearFilter;
+      texture.generateMipmaps = false;
+
+      this.material.uniforms.uTexture.value = texture;
+    })
+
+    this.scene.add(this.mesh);
+    // Gl.scene.add(this);
+
     this.addEvents();
   }
 
-  createMesh() {
-    this.geometry = new THREE.PlaneGeometry(0.666, 0.7, 32, 32);
-    this.material = new THREE.ShaderMaterial({
-      vertexShader,
-      fragmentShader,
-      uniforms: {
-        uTime: { value: 0.0 },
-        uTexture: { value: new THREE.TextureLoader().load(this.img) }
-      },
-      // wireframe: true,
-      side: THREE.DoubleSide
-    });
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.mesh);
+  updateTime(time) {
+    this.material.uniforms.uTime.value = time;
   }
 
   addEvents() {
     window.requestAnimationFrame(this.run.bind(this));
     window.addEventListener("resize", this.onResize.bind(this), false);
+
+    // this.mouseEnter();
+    // this.mouseLeave();
+
+    console.log(this.renderer)
   }
 
   run() {
@@ -214,5 +165,25 @@ export default class Gl extends Vue {
     this.camera.updateProjectionMatrix();
 
     this.renderer.setSize(w, h);
+  }
+
+  mouseEnter() {
+    this.el.addEventListener('mouseenter', () => {
+      gsap.to(this.material.uniforms.uProg, {
+        // duration: 1,
+        value: 1,
+        ease: 'power.inOut',
+      });
+    });
+  }
+
+  mouseLeave() {
+    this.el.addEventListener('mouseleave', () => {
+      gsap.to(this.material.uniforms.uProg, {
+        // duration: 1,
+        value: 0,
+        ease: 'power.inOut',
+      });
+    });
   }
 }
