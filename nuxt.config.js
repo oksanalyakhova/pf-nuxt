6f2d79ab8623ca0fb5809eadd347d7c946995eeb
Oksana Lyakhova <oksana.lyahova@coderiver.com.ua>
2021-04-06 16:15:55 +0300

    updated favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,8 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },]
   },
   css: [
